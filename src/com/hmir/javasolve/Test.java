4624f720fc9d350a2d91f5791fdf4f721297aa86
Hamza Mir <hamzasmir@gmail.com>
2015-11-28 19:07:18 -0500

    comments

--- src/com/hmir/javasolve/Test.java
+++ src/com/hmir/javasolve/Test.java
@@ -3,8 +3,8 @@ package com.hmir.javasolve;
 public class Test {
 
 	public static void main(String[] args) {
-		Expression x = new Expression("log10(10)");
-		System.out.println(x.simplify());
+		Expression x = new Expression("log10(10)"); 
+		System.out.println(x.simplify()); // returns 1
 		
 		System.out.println(Expression.simplify("5^2 * (2 + 3) + cos(0)")); // returns 126.0
 	}
