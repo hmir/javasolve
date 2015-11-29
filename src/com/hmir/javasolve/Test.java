605efe9a2f0703d0155b8e6602a2f8153a9e3c3a
Hamza Mir <hamzasmir@gmail.com>
2015-11-28 19:07:29 -0500

    Update Test.java

--- src/com/hmir/javasolve/Test.java
+++ src/com/hmir/javasolve/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Expression x = new Expression("log10(10)"); 
-		System.out.println(x.simplify()); // returns 1
+		System.out.println(x.simplify()); // returns 1.0
 		
 		System.out.println(Expression.simplify("5^2 * (2 + 3) + cos(0)")); // returns 126.0
 	}
